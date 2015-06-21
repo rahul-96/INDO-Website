994ffad66e35e40fa5ceb5e9a41c7dd6f8506df4
Rahul Abhishek <rahul.28.1996@gmail.com>
2015-06-21 13:41:59 +0530

    INDO Website
    Very basic stage. Under Contruction

---
+++ INDO Hotels/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+      <meta charset="utf-8"/>
+      <title> INDO | Your Ultimate Growth Partner</title>
+      <meta name="viewport" content="width=device-width, initial scale=1.0">
+
+      
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+      <!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+ <style>
+        body {
+          background: #FFFFFF;
+          font-family: verdana, tahoma, Arial, sans-serif;
+          font-size: 18px;
+          overflow: auto;
+        }
+        h1, h2, h3 {
+          font-family: 
+          text-align: center;
+          padding-left: 5%;
+          color: color: #404040;
+        }
+
+        P {
+          padding: 2%;
+          color: color: #404040;
+
+        }
+
+        img{
+          text-align: center;
+          max-width: 100%;
+        }
+        #wrapper{
+
+          margin: 0 auto;
+          max-width: 1180px;
+          width: 98%;
+          background: #FFFFFA;
+          border: 1px solid #B2E6FF;
+          border-radius: 2px;
+          box-shadow: 0px 0px 10px 0px rgba(12, 3, 5, 0.8);
+
+        }
+        #callout{
+          width: 100%;
+          height: auto;
+          background: #1975D1;
+          overflow: hidden;
+          border-radius: 3px;
+        }
+        #callout p{
+          text-align: right;
+          color: #FFFFFA;
+          font-size: 14px;
+          padding: 5px;
+        }
+        #callout p a{
+          color: #FFFFFA;
+          text-decoration: none;
+        }
+        header{
+          width: 98%;
+          min-height: 125px;
+          padding: 5px;
+          text-align: center;
+        }
+        nav{
+          text-align: center;
+        }
+         nav ul{
+          list-style: none;
+          margin: 0;
+          padding-left: 50px;
+
+         }
+
+         nav ul li{
+          float: left;
+          border: 1px solid #B2E6FF;
+          width: 15%;
+         }
+         nav ul li a{
+          background: #FFFFFA; 
+         }
+
+
+      </style>
+</head>
+
+<body>
+	<style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 100%;
+      margin: auto;
+  }
+  </style>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<div id="wrapper">
+		<div id="callout">
+		<p>Call us at: <b>+91-9717658433</b></p>
+		</div>
+		<header>
+			<a href=""><img src="./content/uploads/indologo.jpg" height=95 Width=250 alt="INDO Logo" title="INDO Hotels"></a>
+		</header>
+		<nav>
+			 <ul>
+			     <li class="active"><a href="#">Home</a></li>
+			     <li><a href="goal.php">Our Goal</a></li>
+		         <li><a href="whatwedo.php">What we do?</a></li>
+	        	 <li><a href="howwedoit.php">How we do it?</a></li>
+		         <li><a href="aboutus.php">About Us</a></li>
+		     </ul>
+		</nav>
+		
+
+
+
+
+
+
+
+
+
+		<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="./content/uploads/banner1.jpg" alt="INDO" width="1000" height="480">
+      </div>
+
+      <div class="item">
+        <img src="./content/uploads/banner2.jpg" alt="INDO" width="1000" height="480">
+      </div>
+    
+      <div class="item">
+        <img src="./content/uploads/banner3.jpg" alt="INDO" width="1000" height="480">
+      </div>
+
+      <div class="item">
+        <img src="./content/uploads/banner4.jpg" alt="INDO" width="1000" height="480">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+		<center><img src="./content/uploads/indocover.png" height=90 width=70 ></center>
+		<section class="left=col">
+			<p style="text-indent:50px;">AFFORDABLE</br>
+				With us, there is no need to have a separate budget for accomodation while you travel!</p>
+			<p style="text-indent:50px;">QUALITY ASSURANCE</br>
+                Our team is dedicated to ensure quality enriched stay so that your travel becomes comfortable!</p>
+		    <p style="text-indent:50px;">TECHNOLOGY</br>
+                 Your comfort is our priority. Booking with us is just some clicks away on your mobile or tablet!</p>
+		</section>
+		<aside class="sidebar">
+        <div class="welcome"><img src="./content/uploads/indocover.png" alt="INDO">
+        </div>
+		</aside>
+        <div class="section">
+        	<h3>AFFORDABLE</h3>
+        	<img src="./content/uploads/affordable.png" alt="INDO">
+        	<p>With us, there is no need to have a separate budget for accomodation while you travel!</p>
+        </div>
+        <div class="section">
+        	<h3>QUALITY ASSURANCE</h3>
+        	<img src="./content/uploads/quality.png" alt="INDO">
+        	<p>Our team is dedicated to ensure quality enriched stay so that your travel becomes comfortable</p>
+        </div>
+        <div class="section">
+        	<h3>TECHNOLOGY</h3>
+        	<img src="./content/uploads/technology.png" alt="INDO">
+        	<p>Your comfort is our priority. Booking with us is just some clicks away on your mobile or tablet!</p>
+        </div>
+        <footer>
+        <div class="section">
+        <p>Contact us:</p>
+        <p>contact@indohotels.in</p>
+        <p>+919717658433</p>
+        </div>
+         <div class="section">
+	      <ul>
+            <li><a href="http//www.facebook.com/indohotels"><img src="./content/uploads/facebook.png" height=39 width=39 alt="facebook-plugin"></a></li>
+            <li><a href="http//www.twitter.com/indohotels"><img src="./content/uploads/twitter.png" height=40 width=40 alt="twitter-plugin"></a></li>
+            <li><a href="http//www.linkedin.com/indohotels"><img src="./content/uploads/linkedin.png" height=40 width=40 alt="linkedin-plugin"></a></li>
+            <li><a href="http//www.youtube.com/indohotels"><img src="./content/uploads/youtube.png" height=43 width=43 alt="youtube-plugin"></a></li>
+            <li><a href="http//www.instagram.com/indohotels"><img src="./content/uploads/instagram.png" height=36 width=36 alt="instagram-plugin"></a></li>
+         </ul>
+         </div>
+
+          <div class="section">
+	      <img src="./content/uploads/indocover.png" height=200 width=200  alt="INDO">
+          </div>
+        </footer>
+     </div>
+
+      <p style="text-align:center; padding:0px;">&#169; Copyright - INDO, 2015.</p>
+</body>
+</html>
